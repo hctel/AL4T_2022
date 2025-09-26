450daefae33fca17c3c41796f88e1f02a9307ca5
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-09-26 15:30:44 +0200

    Add StringBuilder example

--- basics/src/main/java/be/ecam/Main.java
+++ basics/src/main/java/be/ecam/Main.java
@@ -3,6 +3,17 @@ package be.ecam;
 public class Main {
 
     public static void main(String[] args) {
-        // So empty...
+        String hello = "hello";
+        String world = "world";
+        System.out.println(hello + " "  +  world);
+
+        StringBuilder builder = new StringBuilder();
+        for( int i =0; i < 100 ; i++) {
+            builder.append(i);
+        }
+        System.out.println(builder.toString());
+
+
+
     }
 }
