a82137e08328000059553ce12b2a3d6a9ed78cc3
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-11-14 14:42:04 +0100

    Use CSV file for parametrized test

--- testing/src/test/java/be/ecam/MathTest.java
+++ testing/src/test/java/be/ecam/MathTest.java
@@ -1,18 +1,14 @@
 package be.ecam;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "2, 3, 5",
-            "-1, 1, 0",
-            "100, 200, 300"
-    })
+    @CsvFileSource(resources = "/addition.csv")
     void testAddition(int a, int b, int expected) {
         assertEquals(expected, a + b);
     }
