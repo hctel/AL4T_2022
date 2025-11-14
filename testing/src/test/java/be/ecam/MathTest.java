d60b46ce5d466f67ff2aab5f2f75bc1ffa7b13d0
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-11-14 14:25:11 +0100

    Add parameterized test

--- testing/src/test/java/be/ecam/MathTest.java
+++ testing/src/test/java/be/ecam/MathTest.java
@@ -1,14 +1,18 @@
 package be.ecam;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/addition.csv")
+    @CsvSource({
+            "2, 3, 5",
+            "-1, 1, 0",
+            "100, 200, 300"
+    })
     void testAddition(int a, int b, int expected) {
         assertEquals(expected, a + b);
     }
