ec8f57bb96efbb0efcffeebfb8616b5b27c4b2be
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-11-14 14:32:58 +0100

    Add Mockito sample

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Mockito core
+    testImplementation("org.mockito:mockito-core:5.13.0")
+// Optional: Mockito + JUnit 5 integration (recommended)
+    testImplementation("org.mockito:mockito-junit-jupiter:5.13.0")
 }
 
 tasks.test {
