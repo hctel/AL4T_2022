ebe39e90cdd1a106844c5e574055e7e3abcff175
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-11-14 14:47:21 +0100

    Create calculator in setup

--- testing/src/test/java/be/ecam/CalculatorTest.java
+++ testing/src/test/java/be/ecam/CalculatorTest.java
@@ -1,49 +1,51 @@
 package be.ecam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     void add() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(1, 2);
         assertEquals(3, result);
     }
 
     @Test
     void add_overflow() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(Integer.MAX_VALUE, 1);
         assertEquals(Integer.MIN_VALUE, result);
     }
 
     @Test
     void subtract() {
-        Calculator calculator = new Calculator();
         int result = calculator.subtract(1, 2);
         assertEquals(-1, result);
     }
 
     @Test
     void multiply() {
-        Calculator calculator = new Calculator();
         int result = calculator.multiply(3, 4);
         assertEquals(12, result);
     }
 
     @Test
     void divide() {
-        Calculator calculator = new Calculator();
         int result = calculator.divide(15, 3);
         assertEquals(5, result);
     }
 
     @Test
     void divide_by_zero() {
-        Calculator calculator = new Calculator();
         assertThrows(ArithmeticException.class, () -> calculator.divide(15, 0));
     }
 
