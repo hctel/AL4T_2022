b3c459730f44694e29477653880b1b0e41d60025
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2025-11-14 13:21:46 +0100

    Create testing module with basic tests

--- testing/src/test/java/be/ecam/CalculatorTest.java
+++ testing/src/test/java/be/ecam/CalculatorTest.java
@@ -1,52 +1,36 @@
 package be.ecam;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    private Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
-    }
-
     @Test
     void add() {
+        Calculator calculator = new Calculator();
         int result = calculator.add(1, 2);
         assertEquals(3, result);
     }
 
     @Test
-    void add_overflow() {
-        int result = calculator.add(Integer.MAX_VALUE, 1);
-        assertEquals(Integer.MIN_VALUE, result);
-    }
-
-    @Test
     void subtract() {
+        Calculator calculator = new Calculator();
         int result = calculator.subtract(1, 2);
         assertEquals(-1, result);
     }
 
     @Test
     void multiply() {
+        Calculator calculator = new Calculator();
         int result = calculator.multiply(3, 4);
         assertEquals(12, result);
     }
 
     @Test
     void divide() {
+        Calculator calculator = new Calculator();
         int result = calculator.divide(15, 3);
         assertEquals(5, result);
     }
-
-    @Test
-    void divide_by_zero() {
-        assertThrows(ArithmeticException.class, () -> calculator.divide(15, 0));
-    }
-
 }
