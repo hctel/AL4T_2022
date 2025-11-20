b51dbc33d1ab6fc29d5d317c34e7fc56f2ac04d9
I42I <119133225+I42I@users.noreply.github.com>
2025-11-21 00:34:12 +0100

    FIX : Violation of DIP of ImageLoader + Sounds overlap (death + background)

--- mario/src/main/java/model/prize/SuperMushroom.java
+++ mario/src/main/java/model/prize/SuperMushroom.java
@@ -4,11 +4,11 @@ import manager.GameEngine;
 import model.hero.Mario;
 import model.hero.MarioForm;
 import view.Animation;
-import view.ImageLoader;
+import view.IImageLoader;
 
 import java.awt.image.BufferedImage;
 
-public class SuperMushroom extends BoostItem{
+public class SuperMushroom extends BoostItem {
 
     public SuperMushroom(double x, double y, BufferedImage style) {
         super(x, y, style);
@@ -19,14 +19,14 @@ public class SuperMushroom extends BoostItem{
     public void onTouch(Mario mario, GameEngine engine) {
         mario.acquirePoints(getPoint());
 
-        ImageLoader imageLoader = new ImageLoader();
+        IImageLoader imageLoader = engine.getImageLoader();
 
-        if(!mario.getMarioForm().isSuper()){
+        if (!mario.getMarioForm().isSuper()) {
             BufferedImage[] leftFrames = imageLoader.getLeftFrames(MarioForm.SUPER);
             BufferedImage[] rightFrames = imageLoader.getRightFrames(MarioForm.SUPER);
 
             Animation animation = new Animation(leftFrames, rightFrames);
-            MarioForm newForm = new MarioForm(animation, true, false);
+            MarioForm newForm = new MarioForm(animation, true, false, imageLoader);
             mario.setMarioForm(newForm);
             mario.setDimension(48, 96);
 
